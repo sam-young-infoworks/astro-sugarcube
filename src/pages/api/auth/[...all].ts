981476ec384b73ef30428b5945c275a292ecd2d8
Sam Young <sam.young@infoworks-tn.com>
2026-06-11 16:07:30 -0500

    update middleware and [...all] routes

--- src/pages/api/auth/[...all].ts
+++ src/pages/api/auth/[...all].ts
@@ -3,6 +3,10 @@ import type { APIRoute } from "astro";
 
 export const prerender = false; // Not needed in 'server' mode
 
+export const GET: APIRoute = async (ctx) => {
+  return auth.handler(ctx.request);
+};
+
 export const ALL: APIRoute = async (ctx) => {
   // If you want to use rate limiting, make sure to set the 'x-forwarded-for' header to the request headers from the context
   // ctx.request.headers.set("x-forwarded-for", ctx.clientAddress);
